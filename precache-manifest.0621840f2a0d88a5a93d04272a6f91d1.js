33f0770844c7ea3283edb621c0f1fc0aa8674c81
degraffa <ard96@cornell.edu>
2020-05-29 12:30:53 -0400

    Updates

---
+++ precache-manifest.0621840f2a0d88a5a93d04272a6f91d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3bea917b9acf1d9178c785938cbab22",
+    "url": "/AlexPointBuy/index.html"
+  },
+  {
+    "revision": "f25bb7f6d13dcec50428",
+    "url": "/AlexPointBuy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "53217e6f64e46d7510bf",
+    "url": "/AlexPointBuy/static/js/2.3f1bb5d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AlexPointBuy/static/js/2.3f1bb5d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f25bb7f6d13dcec50428",
+    "url": "/AlexPointBuy/static/js/main.211bae3f.chunk.js"
+  },
+  {
+    "revision": "20ebd6f210d9bc03971b",
+    "url": "/AlexPointBuy/static/js/runtime-main.b7943eef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AlexPointBuy/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/AlexPointBuy/precache-manifest.0621840f2a0d88a5a93d04272a6f91d1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AlexPointBuy/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
